3ad6b25ac4bac2c2005044ee90f5887926ce1f78
phoolpreet <phoolpreet@gmail.com>
2024-05-12 15:10:44 +0530

    modified:   dlfs/layer_conv2d.py

--- dlfs/layer_conv2d.py
+++ dlfs/layer_conv2d.py
@@ -70,17 +70,22 @@ class Conv2D:
         out_width = int((img_width + np.sum(padding_w) - filter_width) / stride + 1)
         i0 = np.repeat(np.arange(filter_height), filter_width)
         i0 = np.tile(i0, img_channel)
-        print(i0)
+        i1 = stride * np.repeat(np.arange(out_height), out_width)
+        j0 = np.tile(np.arange(filter_width), filter_height * img_channel)
+        j1 = stride * np.tile(np.arange(out_width), out_height)
+        i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+        j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+        k = np.repeat(np.arange(img_channel), filter_height * filter_width).reshape(
+            -1, 1
+        )
+        return i, j, k
 
-    def image_to_column(
+    def determine_padding(
         self,
-        X: np.ndarray,  # [batch, channel, height, width]
         filter_height: int,
         filter_width: int,
         padding: str,
-        stride: int,
     ):
-        # determine padding
         assert padding in ["valid", "same"]
         if padding == "valid":
             pad_h = (0, 0)
@@ -92,10 +97,35 @@ class Conv2D:
             pad_w2 = int(math.ceil((filter_width - 1) / 2))
             pad_h = (pad_h1, pad_h2)
             pad_w = (pad_w1, pad_w2)
+        return pad_h, pad_w
+
+    def output_shape(
+        self,
+        n_filters: int,
+        filter_height: int,
+        filter_width: int,
+        input_height: int,
+        input_width: int,
+        padding: str,
+    ):
+        pad_h, pad_w = self.determine_padding(filter_height, filter_width, padding)
+        output_height = (input_height + np.sum(pad_h) - filter_height) / self.stride + 1
+        output_width = (input_width + np.sum(pad_w) - filter_width) / self.stride + 1
+        return n_filters, int(output_height), int(output_width)
+
+    def image_to_column(
+        self,
+        X: np.ndarray,  # [batch, channel, height, width]
+        filter_height: int,
+        filter_width: int,
+        padding: str,
+        stride: int,
+    ):
+        pad_h, pad_w = self.determine_padding(filter_height, filter_width, padding)
 
         X_padded = np.pad(X, ((0, 0), (0, 0), pad_h, pad_w), mode="constant")
         batch, img_channel, img_height, img_width = X.shape
-        self.get_img2col_indices(
+        i, j, k = self.get_img2col_indices(
             img_channel,
             img_height,
             img_width,
@@ -105,6 +135,11 @@ class Conv2D:
             pad_w,
             stride,
         )
+        cols = X_padded[:, k, i, j]
+        cols = cols.transpose(1, 2, 0).reshape(
+            filter_height * filter_width * img_channel, -1
+        )
+        return cols
 
     def forward(self, X: np.ndarray, is_training: bool):
         assert X.ndim == 4  # [batch size, n_channel, height, width]
@@ -116,12 +151,32 @@ class Conv2D:
         self.input_column = self.image_to_column(
             X, self.filter_height, self.filter_width, self.padding, self.stride
         )
+        self.W_column = self.W.reshape((self.n_filters, -1))
+        output = self.W_column.dot(self.input_column) + self.B
+        output_shape = self.output_shape(
+            self.n_filters,
+            self.filter_height,
+            self.filter_width,
+            x_height,
+            x_width,
+            self.padding,
+        )
+        output = output.reshape(output_shape + (batch_size, ))
+        return output.transpose(3, 0, 1, 2)
 
-    def backward(self, accm_grad: np.ndarray):
-        pass
+    def backward(self, accum_grad: np.ndarray):
+        assert accum_grad.ndim == 4
+        accum_grad = accum_grad.transpose(1, 2, 3, 0).reshape(self.n_filters, -1)
+        if self.is_trainable:
+            W_grad = accum_grad.dot(self.input_column.T).reshape(self.W.shape)
+            B_grad = np.sum(accum_grad, axis=1, keepdims=True)
+            self.W = self.optimizer_W.update(self.W, W_grad)
+            self.B = self.optimizer_B.update(self.B, B_grad)
 
 
 if __name__ == "__main__":
-    conv2d = Conv2D(10, (5, 6), "same", 1, Adam, True)
+    conv2d = Conv2D(10, (5, 6), "same", 2, Adam, True)
     X = np.random.randn(32, 3, 255, 200)
-    conv2d.forward(X, is_training=True)
+    Z = conv2d.forward(X, is_training=True)
+    Z_grad = np.random.randn(*Z.shape)
+    conv2d.backward(Z_grad)
